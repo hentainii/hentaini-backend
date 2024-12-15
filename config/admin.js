00a2c244492b7eb0aa4190c77701e5b3fd1c3c1d
Nikoxx99 <nikoxx9900@gmail.com>
2024-12-15 09:55:44 -0500

    Update strapi version

--- config/admin.js
+++ config/admin.js
@@ -2,4 +2,12 @@ module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET', '2dfc9f2d1e0a9cc7feefb5813b4bcde4'),
   },
+  apiToken: {
+    salt: env('API_TOKEN_SALT', 'f0b0e0a9cc7feefb5813b4bcda4'),
+  },
+  transfer: {
+    token: {
+      salt: env('TRANSFER_TOKEN_SALT'),
+    },
+  },
 });
