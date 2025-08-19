8668512167569949e0ac09622eb3f69445ecc722
Nikoxx99 <nikoxx9900@gmail.com>
2025-08-19 12:54:46 -0500

    refactor(serie): simplify controller by removing custom find logic
    The custom find implementation that calculated newUpdatedAt based on the latest episode was removed, as it's no longer needed. The controller now uses the default createCoreController behavior.

--- src/api/serie/controllers/serie.js
+++ src/api/serie/controllers/serie.js
@@ -6,45 +6,4 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::serie.serie', ({ strapi }) => ({
-  async find(ctx) {
-    // Validar y sanitizar la query
-    await this.validateQuery(ctx);
-    const sanitizedQueryParams = await this.sanitizeQuery(ctx);
-    
-    // Obtener las series usando el servicio
-    const { results, pagination } = await strapi
-      .service('api::serie.serie')
-      .find(sanitizedQueryParams);
-    
-    // Para cada serie, calcular el newUpdatedAt basado en el último episodio
-    const seriesWithNewUpdatedAt = await Promise.all(
-      results.map(async (serie) => {
-        // Buscar el episodio más reciente de esta serie
-        const latestEpisode = await strapi.entityService.findMany('api::episode.episode', {
-          filters: {
-            serie: serie.id
-          },
-          sort: { updatedAt: 'desc' },
-          limit: 1
-        });
-        
-        // Si hay episodios, usar el updatedAt del más reciente, sino usar el de la serie
-        const newUpdatedAt = latestEpisode.length > 0 
-          ? latestEpisode[0].updatedAt 
-          : serie.updatedAt;
-
-        return {
-          ...serie,
-          newUpdatedAt
-        };
-      })
-    );
-    
-    // Sanitizar la salida
-    const sanitizedResults = await this.sanitizeOutput(seriesWithNewUpdatedAt, ctx);
-    
-    // Transformar la respuesta usando el formato estándar de Strapi
-    return this.transformResponse(sanitizedResults, { pagination });
-  }
-}));
+module.exports = createCoreController('api::serie.serie');
