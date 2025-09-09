a9e9151e460f4df62a5995c47bbfb78e7910f0e0
Nikoxx99 <nikoxx9900@gmail.com>
2025-09-09 16:50:50 -0500

    feat(landing-data): add service, routes and controller for landing data
    Implement landing data endpoint that returns featured series, episodes and series

---
+++ src/api/landing-data/controllers/landing-data.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/**
+ * A set of functions called "actions" for `landing-data`
+ * This funcion return 10 carousel series, 20 episodes and 25 series
+ * @returns {Object} - An object with 3 properties: carouselSeries, episodes and series
+ */
+
+
+module.exports = {
+  getLandingData: async (_) => {
+    
+    const carouselSeries = await strapi.entityService.findMany('api::serie.serie', {
+      populate: ['images', 'images.image_type', 'genreList', 'status'],
+      filters: {
+        isFeatured: true,
+      },
+      sort: 'createdAt:desc',
+      limit: 10,
+    });
+
+    const episodes = await strapi.entityService.findMany('api::episode.episode', {
+      populate: ['image', 'image.image_type', 'serie', 'serie.status'],
+      sort: 'createdAt:desc',
+      limit: 20,
+    });
+
+    const series = await strapi.entityService.findMany('api::serie.serie', {
+      populate: ['images', 'images.image_type', 'status'],
+      sort: 'createdAt:desc',
+      limit: 25,
+    });
+
+    const results = {
+      carouselSeries,
+      episodes,
+      series,
+    }
+
+    return results;
+  }
+};
